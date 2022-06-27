711a75fa24671d8d2b5c79fed2a43b46e06833d4
mirim <alfla6528@naver.com>
2022-06-27 16:12:18 +0900

    modifed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import AuthRoute from './pages/AuthRoute';
 
 function App() {
   const authenticated = JSON.parse(localStorage.getItem('user'));
